8e6967d77b125d157dc21495b829ec6571a30637
Bbracken2@mail.greenriver.edu <bbracken2@mail.greenriver.edu>
2020-09-29 16:48:21 -0700

    bbracken

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Beau Bracken</h1>
+      <h2>Hello, I'm Beau Bracken</h2>
+    </div>
+  );
+}
+
+export default App;
